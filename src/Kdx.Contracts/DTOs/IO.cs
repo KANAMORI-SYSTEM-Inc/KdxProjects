24da7556c41129fcf56bf59a059f4585ff5de09d
HayatoShimada <hayato_shimada@kanamori-system.co.jp>
2025-10-06 11:23:45 +0900

    feat:service add

--- src/Kdx.Contracts/DTOs/IO.cs
+++ src/Kdx.Contracts/DTOs/IO.cs
@@ -2,30 +2,30 @@ namespace Kdx.Contracts.DTOs
 {
     public class IO
     {
-        public string Address { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty; // アドレス
 
         public int PlcId { get; set; }
 
         // Properties from KdxDesigner version
         public string? IOText { get; set; }
 
-        public string? XComment { get; set; }
+        public string? XComment { get; set; } // X_Comment
 
-        public string? YComment { get; set; }
+        public string? YComment { get; set; } // Y_Comment
 
-        public string? FComment { get; set; }
+        public string? FComment { get; set; } // F_Comment
 
-        public string? IOName { get; set; }
+        public string? IOName { get; set; } // アクチュエータ名称
 
-        public string? IOExplanation { get; set; }
+        public string? IOExplanation { get; set; } // アクチュエータ説明
 
-        public string? IOSpot { get; set; }
+        public string? IOSpot { get; set; } // ユニット設置場所
 
-        public string? UnitName { get; set; }
+        public string? UnitName { get; set; } // ユニット名称
 
-        public string? System { get; set; }
+        public string? System { get; set; } // 系統
 
-        public string? StationNumber { get; set; }
+        public string? StationNumber { get; set; } // 局番
 
         public string? IONameNaked { get; set; }
 
