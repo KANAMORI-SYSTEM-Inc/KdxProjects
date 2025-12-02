1478d1ad2badf336000ad5a88e9faffc3f5515a8
HayatoShimada <hayato_shimada@kanamori-system.co.jp>
2025-12-02 14:45:55 +0900

    bug: prosTimeDefinitions Dto Upadate

--- src/Kdx.Infrastructure.Supabase/Entities/ProsTimeDefinitionsEntity.cs
+++ src/Kdx.Infrastructure.Supabase/Entities/ProsTimeDefinitionsEntity.cs
@@ -1,21 +1,14 @@
+using Kdx.Contracts.DTOs;
 using Postgrest.Attributes;
 using Postgrest.Models;
-using Kdx.Contracts.DTOs;
 
 namespace Kdx.Infrastructure.Supabase.Entities
 {
     [Table("ProsTimeDefinitions")]
     internal class ProsTimeDefinitionsEntity : BaseModel
     {
-        [PrimaryKey("Id")]
-        [Column("Id")]
-        public int Id { get; set; }
-
         [Column("OperationCategoryId")]
         public int OperationCategoryId { get; set; }
-
-        [Column("TotalCount")]
-        public int TotalCount { get; set; }
 
         [Column("SortOrder")]
         public int SortOrder { get; set; }
@@ -25,18 +18,14 @@ namespace Kdx.Infrastructure.Supabase.Entities
 
         public static ProsTimeDefinitionsEntity FromDto(ProsTimeDefinitions dto) => new()
         {
-            Id = dto.Id,
             OperationCategoryId = dto.OperationCategoryId,
-            TotalCount = dto.TotalCount,
             SortOrder = dto.SortOrder,
             OperationDefinitionsId = dto.OperationDefinitionsId
         };
 
         public ProsTimeDefinitions ToDto() => new()
         {
-            Id = this.Id,
             OperationCategoryId = this.OperationCategoryId,
-            TotalCount = this.TotalCount,
             SortOrder = this.SortOrder,
             OperationDefinitionsId = this.OperationDefinitionsId
         };
